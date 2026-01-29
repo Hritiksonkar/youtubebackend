9f6718d50df0b64f0ab4b8da7c50d1b9775f1ea7
Hritiksonkar <hritiksonkar355@gmail.com>
2026-01-29 13:09:32 +0530

    Fix formatting and add new routes for user profile and author information

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const app= express();
-const path=require('path');
+const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(Path2D.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get("/", function (req, res){
+app.get("/", function (req, res) {
   res.render("index");
 });
- app.listen(3000, function () {
+app.get("/profile/:username", function (req, res) {
+  res.send(`welcome ${req.params.username}`);
+});
+app.get("/author/:username/:age", function (req, res) {
+  res.send(`welcome ${req.params.username}, your age is ${req.params.age}`);
+});
+
+app.listen(3000, function () {
   console.log('its runnig');
 });
