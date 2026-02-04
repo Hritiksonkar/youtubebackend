8d9c3b028cadc683f683c54ffdf03a62e45bcaf2
Hritiksonkar <hritiksonkar355@gmail.com>
2026-02-05 00:24:00 +0530

    feat: add user management features with create and read functionality

--- mongoseCard/app.js
+++ mongoseCard/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 const Path = require('path');
+const User = require('./models/user');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,21 @@ app.use(express.static(Path.join(__dirname, 'public'))) ;
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/read', async (req, res) => {
+  let allusers = await User.find({});
+  
+  res.render('read',{users: allusers});
+});
+
+app.post('/create', async (req, res) => {
+  let{name, email, image} = req.body;
+   let createduser = await User.create({
+     name,
+     email,
+     image
+   }) 
+   res.send(createduser);
 });
 
 app.listen(3000);
