b8cccf3a990616a075f5120b0faf013ee5337f10
Hritiksonkar <hritiksonkar355@gmail.com>
2026-02-06 21:10:45 +0530

    chore: comment out unused JWT authentication code in app.js

--- cookie/app.js
+++ cookie/app.js
@@ -14,5 +14,25 @@ app.get ('/read', function (req, res) {
 });
 
 app.listen(3000);
+// const express = require('express');
+// const app = express();
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const cookieParser = require('cookie-parser');
+
+// app.use(cookieParser());
+
+// app.get("/", function (req, res) {
+//     let token= jwt.sign({email: "user@example.com"}, "secretkey",)
+//         res.cookie("token", token);
+//         res.send("JWT cookie set");
+
+// });
+
+// app.get("/read", function (req, res) {
+//      let data=jwt.verify(req.cookies.token, "secretkey");
+//      console.log(data);
+// });
+// app.listen(3000);
 
 
